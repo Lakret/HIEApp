0e3238815afbd8e69213ac0037657e6939062540
Lakret <Rakuretu@gmail.com>
2015-09-08 22:48:27 +0300

    starting angular part

---
+++ hieApp/web/static/js/hieFormApp.js
@@ -0,0 +1,22 @@
+var hieFormApp = angular.module('hieFormApp', ['ui.router']);
+
+hieFormApp.config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
+  function($locationProvider, $stateProvider, $urlRouterProvider) {
+    $locationProvider.html5Mode(true);
+
+    $stateProvider
+      .state('form', {
+        url: '/',
+        views: {
+          content: {
+            templateUrl: '/views/index.html',
+            controller: 'IndexCtrl'
+          }
+        },
+      });
+  }]);
+// .run(['$rootScope', function($rootScope) {
+//     $rootScope.$on('$stateChangeSuccess',function (event, toState, toParams, fromState, fromParams){
+//       $rootScope.bodyClass = toState.bodyClass;
+//     });
+//   }]);
